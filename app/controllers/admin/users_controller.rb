52de24188ed9123f552c0f1c53231c34c7b7de12
Thiago Nogueira <nogueirathiago@gmail.com>
2014-10-01 18:01:36 +0000

    admins

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,21 +1,10 @@
 class Admin::UsersController < ApplicationController
  
 before_action :authorize
-  
 
   def index
-    redirect_to movies_path if @current_user.admin?
-
     @users = User.all
   end
-
-  # def authorize
-  #   unless current_user && current_user.admin
-  #     flash[:error] = 'You have to be an admin to access this page.'
-  #     redirect_to :root
-  #   end
-    
-  # end
 
 
 end
