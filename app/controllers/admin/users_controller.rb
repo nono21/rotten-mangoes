bee3719befcf3c9ff28fbe687ca130a4f3032ab0
Thiago Nogueira <nogueirathiago@gmail.com>
2014-10-02 19:10:37 +0000

    ...

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,8 +3,28 @@ class Admin::UsersController < ApplicationController
 before_action :authorize
 
   def index
-    @users = User.all.page(params[:page]).per(2)
+    @users = User.all.page(params[:page]).per(5)
   end
 
+  def new
+    @user = User.new
+  end
 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to admin_users_path, notice: "The user #{@user.firstname} was created with success!"
+    else
+      render :new
+    end
+  end
+
+  protected
+  def user_params
+    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation)
+    
+  end
 end
+
